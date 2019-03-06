7abfd801dbcf2abc4eb0c8d541d9c9315228085d
zhangyuhong <289590351@qq.com>
2019-03-06 17:35:34 +0800

    【使用xUtils访问网络数据以及使用Gson解析Json】 xUtils和Json都是两个jar包，需要导入

--- app/src/main/java/com/hong/zyh/wisdombj/pager/impl/NewsCenterPager.java
+++ app/src/main/java/com/hong/zyh/wisdombj/pager/impl/NewsCenterPager.java
@@ -2,11 +2,22 @@ package com.hong.zyh.wisdombj.pager.impl;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.hong.zyh.wisdombj.global.GlobalConstants;
 import com.hong.zyh.wisdombj.pager.BasePager;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+
+import domain.NewsMenu;
 
 
 /**
@@ -17,7 +28,9 @@ import com.hong.zyh.wisdombj.pager.BasePager;
  */
 public class NewsCenterPager extends BasePager {
 
-	public NewsCenterPager(Activity activity) {
+    private NewsMenu mNewsData;
+
+    public NewsCenterPager(Activity activity) {
 		super(activity);
 	}
 
@@ -39,6 +52,46 @@ public class NewsCenterPager extends BasePager {
 
 		// 显示菜单按钮
 		btnMenu.setVisibility(View.VISIBLE);
+
+        // 请求服务器,获取数据
+        // 开源框架: XUtils
+        getDataFromServer();
 	}
 
+	/**
+	 * 从服务器获取数据 需要权限:<uses-permission android:name="android.permission.INTERNET"
+	 * />
+	 */
+	private void getDataFromServer() {
+		HttpUtils utils = new HttpUtils();
+		//参数1：请求方式 参数2：uil地址   参数3：回调(返回的是json的字符串数据，因此繁星应该是String)
+        //HttpMethod导入的方法是：com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+		utils.send(HttpMethod.GET, GlobalConstants.CATEGORY_URL, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                // 请求成功
+                String result = responseInfo.result;// 获取服务器返回结果
+                Log.d("服务器返回结果:",result);
+                //开始解析数据
+                processData(result);
+            }
+
+            @Override
+            public void onFailure(HttpException error, String msg) {
+                 // 请求失败
+                error.printStackTrace();
+                Log.d("服务器返回error:",msg);
+                Toast.makeText(mActivity, msg, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+	}
+
+    protected void processData(String json) {
+        Gson gson= new Gson();
+        //mNewsData已经把所有的字段填充好了
+        //参数1：jon原数据，相当于字符串  参数2：封装json数据的类
+        mNewsData = gson.fromJson(json, NewsMenu.class);
+        Toast.makeText(mActivity,mNewsData.toString(),Toast.LENGTH_SHORT).show();
+    }
 }
