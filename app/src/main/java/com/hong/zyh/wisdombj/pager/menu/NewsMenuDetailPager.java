2b0901a5ab4fc85e9387b859801997df74ba8ebc
zhangyuhong <289590351@qq.com>
2019-03-08 14:41:05 +0800

    【viewPagerIndicator指示器的使用，就是页面标签的滑动】 方法：  1.引入库  2.解决support-v4冲突(让两个版本一致,androidstudio不一定要修改)  3.从例子程序中拷贝布局文件  4.从例子程序中拷贝相关代码(指示器和viewpager绑定; 重写getPageTitle返回标题)  5.在清单文件中增加样式  6.背景修改为白色  7.修改样式-背景样式&文字样式 	modified:   .gitignore 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/hong/zyh/wisdombj/pager/menu/NewsMenuDetailPager.java 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/layout/pager_news_menu_detail.xml

--- app/src/main/java/com/hong/zyh/wisdombj/pager/menu/NewsMenuDetailPager.java
+++ app/src/main/java/com/hong/zyh/wisdombj/pager/menu/NewsMenuDetailPager.java
@@ -13,6 +13,7 @@ import com.hong.zyh.wisdombj.R;
 import com.hong.zyh.wisdombj.pager.BaseMenuDetailPager;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
+import com.viewpagerindicator.TabPageIndicator;
 
 import java.util.ArrayList;
 
@@ -21,13 +22,24 @@ import domain.NewsMenu;
 
 /**
  * 菜单详情页-新闻
- * 
+ *  ViewPagerIndicator使用流程:
+ *  1.引入库
+ *  2.解决support-v4冲突(让两个版本一致)
+ *  3.从例子程序中拷贝布局文件
+ * 4.从例子程序中拷贝相关代码(指示器和viewpager绑定; 重写getPageTitle返回标题)
+ * 5.在清单文件中增加样式
+ * 6.背景修改为白色
+ * 7.修改样式-背景样式&文字样式
+ *
  * Created by shuaihong on 2019/3/7.
  */
 public class NewsMenuDetailPager extends BaseMenuDetailPager {
 
 	@ViewInject(R.id.vp_news_menu_detail)
 	ViewPager mViewPager;
+
+	@ViewInject(R.id.indicator)
+	private TabPageIndicator mIndicator;
 
 	private ArrayList<TabDetailPager> mPagers;// 页签页面集合
 
@@ -56,9 +68,18 @@ public class NewsMenuDetailPager extends BaseMenuDetailPager {
 		}
 
 		mViewPager.setAdapter(new NewsMenuDetailAdapter());
+		// 将viewpager和指示器绑定在一起.注意:必须在viewpager设置完数据之后再绑定
+		mIndicator.setViewPager(mViewPager);
 	}
 
 	class NewsMenuDetailAdapter extends PagerAdapter {
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			// 指定指示器的标题
+			NewsMenu.NewsTabData newsTabData = mTabData.get(position);
+			return newsTabData.title;
+		}
 
 		@Override
 		public int getCount() {
